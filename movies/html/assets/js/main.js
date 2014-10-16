0eaf4e2d986e6d88bcc22d19f21cbab4ef2c943b
David Exelby <david@sulaco.co.uk>
2014-10-16 20:28:00 +0100

    bower

--- movies/html/assets/js/main.js
+++ movies/html/assets/js/main.js
@@ -1,31 +1,29 @@
-require(["config"], function() {
+require(["config"], function () {
+    "use strict"
     require(["app", "router", "helper/state",
              "views/header", "views/movie/search",
              "models/movie/summary",
              "models/user/user", "bootstrap"],
-        function(app, Router, State, HeaderView, MovieSearchView,
+        function (app, Router, State, HeaderView, MovieSearchView,
                  movieSummary, user, $) {
+            $('#javascript_alert').remove();
 
-        "use strict"
+            //FIX ME::Need to get genre types and initial counts
 
-        $('#javascript_alert').remove();
+            user.fetch({}).done(function () {
 
-        //FIX ME::Need to get genre types and initial counts
+                State.reset(true);
+                app.router = new Router();
 
-        user.fetch({}).done(function() {
+                new HeaderView({model: user});
+                new MovieSearchView({model: movieSummary});
 
-            State.reset(true);
-            app.router = new Router();
+                Backbone.history.start({pushState: false, root: app.root});
 
-            new HeaderView({model: user});
-            new MovieSearchView({model:movieSummary});
+                setInterval(function () {
+                    user.poll();
+                }, 10000);
 
-            Backbone.history.start({pushState: false, root:app.root});
-
-            setInterval(function(){
-                user.poll();
-            }, 10000);
-
+            });
         });
-    });
 });
